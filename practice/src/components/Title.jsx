de71796eae85da4397473e95b21f37dd077bcb11
JaydenLee1116 <vv55adss@gmail.com>
2023-05-28 10:07:38 +0900

    feat: 컴포넌트 함수 호출 내용 추가

--- practice/src/components/Title.jsx
+++ practice/src/components/Title.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 const Title = (props) => {
+  const [state, setState] = useState(0); // [상태값, 상태를 변경하는 함수
   const {children, color} = props;
   return (
     <div>
       <h2 style={{color}}>타이틀 컴포넌트입니당</h2>
+      <div>{state}</div>
+      <button onClick={() => setState(state + 1)}></button>
       {children}
     </div>
   );
